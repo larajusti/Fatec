2d77ccd24aca411aaa4823bc469fea03753d6846
LucasJusti <lucas91js@gmail.com>
2020-05-14 22:25:04 -0300

    added select api

--- consumoAPI/index.php
+++ consumoAPI/index.php
@@ -1,17 +1,40 @@
-<?php
-    include_once('postmon.php');
-    $address = getAddress();
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consumindo API</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <form action="." method="post">
+        <select name="selectApi">
+            <option value="viacep">Viacep</option>
+            <option value="postmon">Postmon</option>
+            <option value="cepaberto">Cep Aberto</option>
+        </select>
+        <?php
+        if (isset($_POST['selectApi'])) {
+            $option = $_POST['selectApi'];
+        }
+        else {
+            $option = 'viacep';
+        }
+    
+        if ($option == 'viacep') {
+            include_once('viacep.php');
+        }
+
+        if ($option == 'postmon') {
+            include_once('postmon.php');
+        }
+        if ($option == 'cepaberto') {
+            include_once('cepAberto.php');
+        }
+        $address = getAddress();
+        ?>
         <p>Digite o CEP para encontrar o endereço.</p>
         <input type="text" placeholder="Digite um cep..." name="cep" value="<?php echo $address->cep ?>">
         <input type="submit">
@@ -20,5 +43,7 @@
         <input type="text" placeholder="cidade" name="cidade" value="<?php echo $address->localidade ?>">
         <input type="text" placeholder="estado" name="estado" value="<?php echo $address->uf ?>">
     </form>
+
 </body>
+
 </html>
